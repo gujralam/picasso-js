f0b180db46a8ccc7ad49571c8086a7b41d689688
Amit Gujral <gujralam@gmail.com>
2020-09-05 07:58:22 +1000

    Fix for (node:14260) [DEP0016] DeprecationWarning: 'GLOBAL' is deprecated, use 'global'

--- Picasso.js
+++ Picasso.js
@@ -9,14 +9,14 @@
 //Why white? I'm just assuming that's your default, if not, just set another color! 
 //Want to give it a try? Invoke this file doing "node Picasso.js test" from terminal/CMD
 
-GLOBAL.red   = '\033[31m';
-GLOBAL.green   = '\033[32m';
-GLOBAL.yellow  = '\033[33m';
-GLOBAL.blue   = '\033[34m';
-GLOBAL.magenta  = '\033[35m';
-GLOBAL.cyan  = '\033[36m';
-GLOBAL.white = '\033[0m';
-GLOBAL.black   = '\033[30m';
+global.red   = '\033[31m';
+global.green   = '\033[32m';
+global.yellow  = '\033[33m';
+global.blue   = '\033[34m';
+global.magenta  = '\033[35m';
+global.cyan  = '\033[36m';
+global.white = '\033[0m';
+global.black   = '\033[30m';
 
 var comando = process.argv[2];
 if (comando == "test") {
